1ab44024330b6989de3d7d336ec68106209a4cd3
lucianozapata <60700288+lucianozapata@users.noreply.github.com>
2022-02-09 16:39:21 +0100

    feat: Parse POST request from Github Hook (#19)
    * feat: Implemented body-parser
    
    * feat: Parsed the commit sha
    
    * feat: Added JSON dependencies. #2
    
    * feat: Parsed parameters from POST request issue#2

--- src/main/java/com/group24/CI/ContinuousIntegrationServer.java
+++ src/main/java/com/group24/CI/ContinuousIntegrationServer.java
@@ -3,12 +3,18 @@ package com.group24.CI;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
 import java.io.IOException;
  
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.util.IO;
+import org.eclipse.jetty.util.ajax.JSON;
+import org.eclipse.jetty.util.ajax.JSONEnumConvertor;
+import org.eclipse.jetty.util.ajax.JSONObjectConvertor;
+import org.json.JSONObject;
+
 
 /** 
  Skeleton of a com.group24.CI.ContinuousIntegrationServer which acts as webhook
@@ -25,10 +31,34 @@ public class ContinuousIntegrationServer extends AbstractHandler
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
+        String stringObject = IO.toString(request.getReader());
+        JSONObject body = new JSONObject(stringObject);
 
-        System.out.println(target);
+        if(request.getMethod() == "POST"){
 
-        // here you do all the continuous integration tasks
+            // Parse the repository URL
+            String repository_url = body.getJSONObject("repository").getString("html_url");
+            // Parse the commit SHA (secure hashing algorithm)
+            String commit_hash = body.getString("after");
+            //Parse the project name
+            String repository_name = body.getJSONObject("repository").getString("full_name");
+
+            // Parse the date & time the push happened.
+            String updated_at = body.getJSONObject("repository").getString("updated_at");
+
+        }
+
+
+
+
+
+
+
+        // Parse the commit message
+        //String branch_name = body.getJSONArray("commit").getString("")
+
+
+        // here you do all the continuous integration task
         // for example
         // 1st clone your repository
         // 2nd compile the code
