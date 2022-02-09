cb299c0dfef667b5a39b275fd00332802a907697
avatar46 <90114456+avatar46@users.noreply.github.com>
2022-02-09 21:58:42 +0100

    Fix: CloneRepository class(#21) (#22)
    The class will now clone the repo under the folder named with the repo's name.
    The class will clean up the local folder before cloning.
    modified the CloneRepositoryTest accordingly.

--- src/test/java/com/group24/CI/CloneRepositoryTest.java
+++ src/test/java/com/group24/CI/CloneRepositoryTest.java
@@ -1,14 +1,6 @@
 package com.group24.CI;
 
-import org.gradle.internal.impldep.org.apache.commons.io.FileUtils;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,17 +10,7 @@ public class CloneRepositoryTest {
     String repoUrl = "https://github.com/lucianozapata/DD2480VT221.git";
     // The local path of the clone destination.
     String projectPath = System.getProperty("user.dir"); // get the path of the project
-    String directoryPath = String.valueOf(Paths.get(projectPath, "clone_test"));
-
-    /**
-     * Deletes the repository under the directory path before and after the test.
-     * @throws IOException
-     */
-    @BeforeEach
-    @AfterEach
-    void clearRepository() throws IOException {
-        FileUtils.deleteDirectory(new File(directoryPath));
-    }
+    String repoName = "DD2480VT221";
 
     /**
      * Test if the method can clone a repo to a designated directory
@@ -36,7 +18,7 @@ public class CloneRepositoryTest {
     @Test
     void testCloneRepository() {
 
-        CloneRepository clone = new CloneRepository(repoUrl, directoryPath);
+        CloneRepository clone = new CloneRepository(repoUrl, projectPath, repoName);
         assertTrue(clone.cloneRepository());
     }
 }
