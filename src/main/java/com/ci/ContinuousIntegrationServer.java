c6979d674e3141eca18e240ff81b5a7f1dad2722
Samuel Falk <falkwastaken@gmail.com>
2023-02-05 18:56:00 +0100

    Add method to set commit status
    Add a generic method that sets the commit status for the current SHA.

--- src/main/java/com/ci/ContinuousIntegrationServer.java
+++ src/main/java/com/ci/ContinuousIntegrationServer.java
@@ -2,10 +2,13 @@ package com.ci;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.ServletException;
- 
+
+import java.io.DataOutputStream;
 import java.io.IOException;
- 
+import java.net.URL;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -18,6 +21,17 @@ public class ContinuousIntegrationServer extends AbstractHandler
 {  
     final static int GROUP_NUMBER = 31;
     final static int PORT = 8000 + GROUP_NUMBER;
+
+    String repOwner;
+    String repName;
+    String sha;
+
+    enum CommitStatus {
+        error,
+        failure,
+        pending,
+        success
+    }
     
     public void handle(String target,
                        Request baseRequest,
@@ -53,9 +67,31 @@ public class ContinuousIntegrationServer extends AbstractHandler
     private void build() {
 
     }
+    
+    private void postStatus(CommitStatus status, String description) throws Exception {
+        // API enpoint for setting the commit status  
+        URL url = new URL(String.format("https://api.github.com/repos/%s/%s/statuses/%s", repOwner, repName, sha));
+        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("accept", "application/vnd.github+json"); // Recommended header
+        con.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
 
-    private void report() {
+        // Add status and description to body:
+        out.writeBytes(String.format("\"status\":\"%s\", \"description\":\"%d\"", status, description));
+        out.flush();
+        out.close();
 
+        // Send request
+        con.connect();
+
+        // Check response code
+        int code = con.getResponseCode();
+        if (code != 200) {
+            System.out.println(String.format("Error when setting commit status! (%d)", code));
+            throw new Exception(con.getResponseMessage());
+        }
+        con.disconnect();
     }
 
     private void cleanup() {
