2594cefdfc0b6ae7f5411f6e22a26ed54131df11
sebvei <54596049+sebvei@users.noreply.github.com>
2023-02-06 19:14:58 +0100

    Removed preliminary test.

--- src/test/java/com/ci/ContinuousIntegrationServerTest.java
+++ src/test/java/com/ci/ContinuousIntegrationServerTest.java
@@ -12,26 +12,4 @@ public class ContinuousIntegrationServerTest {
         public void verifyNoExceptionsThrown(){
             DEFAULT.gradleTest();
         }
-
-        @Test
-        /*
-         * Requirement: cloneRepo successfully runs the "git clone repoName dirPath" command.
-         * Precondition (may need update): 
-         *      Directory path is valid
-         *      Repository URL is valid
-         *      The CI server has access to the repository (may clone, has internet access etc.)
-         *       
-         * Postcondition: 
-         *      The exit code is 0.
-         */
-        public void cloneRepo_Correct_Return() throws IOException, InterruptedException{
-            int exitValue = DEFAULT.publicCloneRepo();
-            assertEquals(0, exitValue);
-        }
-
-
-
-
-
 }
-
